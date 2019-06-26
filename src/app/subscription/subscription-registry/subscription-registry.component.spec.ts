c91a78a2c8c17b694a94404dec0a5b3b684d9660
psadasivam <psadasivam@manh.com>
2019-06-27 02:51:04 +0530

    Added unit test cases

--- src/app/subscription/subscription-registry/subscription-registry.component.spec.ts
+++ src/app/subscription/subscription-registry/subscription-registry.component.spec.ts
@@ -1,6 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SubscriptionRegistryComponent } from './subscription-registry.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatSelectModule } from '@angular/material';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, Params } from '@angular/router';
+import { SubscriptionRegistryService } from './subscription-registry.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { By } from '@angular/platform-browser';
 
 describe('SubscriptionRegistryComponent', () => {
   let component: SubscriptionRegistryComponent;
@@ -8,9 +15,31 @@ describe('SubscriptionRegistryComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SubscriptionRegistryComponent ]
+      declarations: [SubscriptionRegistryComponent],
+      imports: [
+        BrowserAnimationsModule,
+        MatSelectModule,
+        MatInputModule,
+        MatFormFieldModule,
+        MatIconModule,
+        MatButtonModule,
+        CommonModule,
+        ReactiveFormsModule
+      ],
+      providers: [
+        {
+          provide: ActivatedRoute, useValue: {
+            queryParams: {
+              subscribe: (fn: (value: Params) => void) => fn({
+                subscriptionModel: 'basic'
+              }),
+            }
+          }
+        },
+        SubscriptionRegistryService
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -21,5 +50,18 @@ describe('SubscriptionRegistryComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Remove sign must be disabled for the 1st input box', () => {
+    const formElement = fixture.debugElement.query(By.css('#ip-input-form'));
+    const inputElement = formElement.nativeElement.firstElementChild;
+    expect(inputElement.children[1].classList.contains('disabled')).toBe(true);
+  });
+
+  it('Add sign must be enabled for the 1st input box only when there are no other input fields present', () => {
+    const formElement = fixture.debugElement.query(By.css('#ip-input-form'));
+    const inputElement = formElement.nativeElement.firstElementChild;
+    expect(inputElement.children[2].classList.contains('disabled')).toBe(false);
+    expect(formElement.children.length).toBe(1);
   });
 });
